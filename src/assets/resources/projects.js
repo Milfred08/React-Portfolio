ff01378ab8aa1546c9cebd2de72195226d9965c3
milfred08 <milfred08@gmail.com>
2022-09-04 18:24:46 -0400

    added more screenshots

--- src/assets/resources/projects.js
+++ src/assets/resources/projects.js
@@ -35,37 +35,39 @@ const projects = [
        // githubUrl: "//",
        // deployedUrl: "---"
     //},
+
     {
         id: 4,
+        title: "Vacation Station",
+        key: "vacation-station",
+        description: "All your needs for foods and rental in one place",
+        date: "August 2022",
+        languages: "Interactive Full-Stack Project",
+        image: "../../assets/images/screenshot-vacation-station.PNG",
+        imageDescription: "Screenshot of Vacation Station App",
+        githubUrl: "https://github.com/maddyrae9/vacation-station/",
+        deployedUrl: "https://vacationstation.herokuapp.com/"
+    },
+    {
+        id: 5,
         title: "Budget Tracker",
         key: "budget-tracker",
         description: "App for tracking budget and logging savings and withdrawals.",
         date: "August 2022",
-        langauges: "Node.js // Express.js // MongoDB, Mongoose // IndexDB",
+        languages: "Node.js // Express.js // MongoDB, Mongoose // IndexDB",
         image: "../../assets/images/budget-tracker.PNG",
         imageDescription: "Screenshot of Budget Tracker App",
         githubUrl: "https://github.com/Milfred08/BudgetTracker/",
         deployedUrl: "https://budgetracker-19.herokuapp.com/"
     },
-    {
-        id: 5,
-        title: "Vacation Station",
-        key: "vacation-station",
-        description: "All your needs for foods and rental in one place",
-        date: "August 2022",
-        langauges: "Interactive Full-Stack Project",
-        image: "../../assets/images/vacation-station.PNG",
-        imageDescription: "Screenshot of Vacation Station App",
-        githubUrl: "https://github.com/maddyrae9/vacation-station/",
-        deployedUrl: "https://vacationstation.herokuapp.com/"
-    },
+  
     {
         id: 6,
         title: "Code Quiz",
         key: "code-quiz",
         description: "5-question quiz to test your javascript knowledge.",
         date: "May 2022",
-        langauges: "Javascript // HTML // CSS",
+        languages: "Javascript // HTML // CSS",
         image: "../../assets/images/Portfolio-Screenshot-code-quiz.PNG",
         imageDescription: "Screenshot of Code Quiz",
         githubUrl: "https://github.com/Milfred08/Javascript-web-apiQuiz",
@@ -77,7 +79,7 @@ const projects = [
         key: "run-buddy",
         description: "Project Summary: Developed a website for a fitness comany using HTML and CSS",
         date: "April 2022",
-        langauges: "HTML & CSS",
+        languages: "HTML & CSS",
         image: "../../assets/images/Portfolio-Screenshot-run-buddy.PNG",
         imageDescription: "Screenshot of Run Buddy website",
         githubUrl: "https://github.com/Milfred08/runbuddy/",
